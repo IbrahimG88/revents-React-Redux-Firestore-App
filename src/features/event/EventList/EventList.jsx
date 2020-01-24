6b97c54862a65ec136f1efc2d62e2a7acb4b241d
IbrahimG88 <goharyibrahim@gmail.com>
2020-01-24 14:59:19 +0200

    Passing down static props to components

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -1,13 +1,22 @@
 import React, { Component, Fragment } from 'react'
 import EventListItem from './EventListItem'
 
+/**
+ * we will loop over the events array that was passed as  a prop
+ * we will the EventListItem an id as events.id
+ * we will use the event prop as this.props.event in the 
+ * EventListItem component as you can see there
+ */
+
 class EventList extends Component {
     render() {
         return (
             <Fragment>
-                <EventListItem />
-                <EventListItem />
-                <EventListItem />
+                {this.props.events.map(event => (
+                 <EventListItem key={event.id} 
+                 event= {event}/>   
+                ))}
+               
             </Fragment>
         )
     }
